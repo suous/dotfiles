1f25e1f985c8e205686db60b54802546bde01874
suous <111222500+suous@users.noreply.github.com>
2023-05-22 17:45:21 +0800

    Added alacritty and tmux configs.

---
+++ exact_dot_oh-my-zsh/exact_custom/aliases.zsh
@@ -0,0 +1,8 @@
+alias aliases="${EDITOR:-vim} $ZSH_CUSTOM/aliases.zsh"
+alias custom="${EDITOR:-vim} $ZSH_CUSTOM/custom.zsh"
+
+alias c=clear
+
+[ -x "$(command -v exa)" ] && alias ls=exa
+[ -x "$(command -v bat)" ] && alias cat=bat
+
---
+++ exact_dot_oh-my-zsh/exact_custom/custom.zsh
@@ -0,0 +1,2 @@
+[ -x "$(command -v starship)" ] && eval "$(starship init zsh)"
+[ -x "$(command -v zoxide)" ] && eval "$(zoxide init zsh)"
